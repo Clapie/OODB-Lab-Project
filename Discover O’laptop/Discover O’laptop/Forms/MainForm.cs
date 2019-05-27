542a15ba00be97a0bdf93f07117bdc808f5553c2
Clarissa Arifin <clapiegouw@gmail.com>
2019-05-27 17:47:44 +0700

    Merge MemberMainForm and StaffMainForm, Change MainForm to MDI

---
+++ Discover O’laptop/Discover O’laptop/Forms/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using Discover_O_laptop.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discover_O_laptop
+{
+    public partial class Form1 : Form
+    {
+        LoginForm loginForm = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            logout();
+        }
+
+        public void loginAdmin()
+        {
+            loginToolStripMenuItem.Visible = false;
+            logoutToolStripMenuItem.Visible = true;
+        }
+
+        public void loginMember()
+        {
+
+        }
+
+        public void logout()
+        {
+            loginToolStripMenuItem.Visible = true;
+            logoutToolStripMenuItem.Visible = false;
+            changePasswordToolStripMenuItem.Visible = false;
+            buyToolStripMenuItem.Visible = false;
+            manageToolStripMenuItem.Visible = false;
+            viewToolStripMenuItem.Visible = false;
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loginForm == null || loginForm.IsDisposed == true)
+            {
+                loginForm = new LoginForm();
+                loginForm.MdiParent = this;
+                loginForm.Show();
+            }
+        }
+    }
+}
